e65c6668932832e94d68b849700e6cfe9b520397
Hari Narayanan <97302578+Hari-1903@users.noreply.github.com>
2022-08-08 15:43:25 +0530

    Add files via upload

---
+++ JavaScript/javascript.js
@@ -0,0 +1,7 @@
+"use strict";
+
+document.querySelector(".menu-button").addEventListener("click", function () {
+  document.querySelector(".head-pointers").classList.toggle("open");
+});
+
+console.log("Ha");
